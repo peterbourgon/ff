788961c6d994738adb5aac45db54fa5e2e3c9898
Peter Bourgon <peter@bourgon.org>
2019-08-06 12:32:09 -0700

    ffcli: Command: clarify Options precedence

--- ffcli/command.go
+++ ffcli/command.go
@@ -24,6 +24,11 @@ type Command struct {
 // ParseAndRun parses the commandline arguments for this command and all
 // subcommands recursively, and executes the main function for the terminal
 // command, if it's defined.
+//
+// Any options passed to ParseAndRun are provided to every visited command
+// during ff.Parse. To provide options to only a specific command, declare them
+// in the Options field for that command. Options declared for specific commands
+// take precedence over options passed to ParseAndRun.
 func (c *Command) ParseAndRun(args []string, options ...ff.Option) error {
 	if err := ff.Parse(c.FlagSet, args, append(options, c.Options...)...); err != nil {
 		return err
