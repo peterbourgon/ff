65a9156d70ef5144f74a73713d833c66da5c692e
Peter Bourgon <peter@bourgon.org>
2019-09-03 12:17:36 -0700

    ffyaml: move test configs to testdata

--- ffyaml/ffyaml_test.go
+++ ffyaml/ffyaml_test.go
@@ -17,60 +17,51 @@ func TestParser(t *testing.T) {
 	}{
 		{
 			name: "empty input",
-			file: ``,
+			file: "testdata/empty_input.yaml",
 			want: fftest.Vars{},
 		},
 		{
 			name: "basic KV pairs",
-			file: "s: hello\ni: 10\nb: true\nd: 5s\nf: 3.14",
+			file: "testdata/basic.yaml",
 			want: fftest.Vars{S: "hello", I: 10, B: true, D: 5 * time.Second, F: 3.14},
 		},
 		{
 			name: "invalid prefix",
-			file: "\ti: 123\ns: foo\n",
+			file: "testdata/invalid_prefix.yaml",
 			want: fftest.Vars{WantParseErrorString: "found character that cannot start any token"},
 		},
 		{
 			name: "no value",
-			file: "i: 123\ns:\n",
+			file: "testdata/no_value.yaml",
 			want: fftest.Vars{WantParseErrorIs: ff.StringConversionError{}},
 		},
 		{
-			name: "no file",
-			file: ``,
-			want: fftest.Vars{},
-		},
-		{
 			name: "basic arrays",
-			file: "s: ['a', 'b', 'c']\n\nx: ['a', 'b', 'c']",
+			file: "testdata/basic_array.yaml",
 			want: fftest.Vars{S: "c", X: []string{"a", "b", "c"}},
 		},
 		{
 			name: "multiline arrays",
-			file: "s:\n  - a\n  - b\n  - c\nx:\n  - d\n  - e\n  - f\n",
+			file: "testdata/multi_line_array.yaml",
 			want: fftest.Vars{S: "c", X: []string{"d", "e", "f"}},
 		},
 		{
 			name: "line break arrays",
-			file: "x: [\"first string\", \"second\n string\", \"third\"]\n",
+			file: "testdata/line_break_array.yaml",
 			want: fftest.Vars{X: []string{"first string", "second string", "third"}},
 		},
 		{
 			name: "unquoted strings in arrays",
-			file: "x: [one, two, three]",
+			file: "testdata/unquoted_string_array.yaml",
 			want: fftest.Vars{X: []string{"one", "two", "three"}},
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
-			filename, cleanup := fftest.TempFile(t, testcase.file)
-			defer cleanup()
-
 			fs, vars := fftest.Pair()
 			vars.ParseError = ff.Parse(fs, []string{},
-				ff.WithConfigFile(filename),
+				ff.WithConfigFile(testcase.file),
 				ff.WithConfigFileParser(ffyaml.Parser),
 			)
-
 			if err := fftest.Compare(&testcase.want, vars); err != nil {
 				t.Fatal(err)
 			}
