eb03ae85eed5d3aed7d54ffd254f8183891abcd4
Peter Bourgon <peter@bourgon.org>
2019-09-03 12:08:29 -0700

    {Plain,JSON}Parser: move test configs to testdata

--- parse_test.go
+++ parse_test.go
@@ -29,7 +29,7 @@ func TestParseBasics(t *testing.T) {
 		},
 		{
 			name: "file only",
-			file: "s bar\ni 99\nb true\nd 1h",
+			file: "testdata/1.conf",
 			want: fftest.Vars{S: "bar", I: 99, B: true, D: time.Hour},
 		},
 		{
@@ -39,7 +39,7 @@ func TestParseBasics(t *testing.T) {
 		},
 		{
 			name: "file and args",
-			file: "\ns should be overridden\n\nd 3s\n",
+			file: "testdata/2.conf",
 			args: []string{"-s", "foo", "-i", "1234"},
 			want: fftest.Vars{S: "foo", I: 1234, D: 3 * time.Second},
 		},
@@ -52,13 +52,13 @@ func TestParseBasics(t *testing.T) {
 		{
 			name: "env and file",
 			env:  map[string]string{"TEST_PARSE_S": "should be overridden", "TEST_PARSE_B": "true"},
-			file: "s bar\ni 99\n\nd 34s\n\n # comment line\n",
+			file: "testdata/3.conf",
 			want: fftest.Vars{S: "bar", I: 99, B: true, D: 34 * time.Second},
 		},
 		{
 			name: "env file args",
 			env:  map[string]string{"TEST_PARSE_S": "from env", "TEST_PARSE_I": "300", "TEST_PARSE_F": "0.15", "TEST_PARSE_B": "true", "TEST_PARSE_D": "1h"},
-			file: "s from file\ni 200 # comment\n\nd 1m\nf 2.3\n\n",
+			file: "testdata/4.conf",
 			args: []string{"-s", "from arg", "-i", "100"},
 			want: fftest.Vars{S: "from arg", I: 100, F: 2.3, B: true, D: time.Minute},
 		},
@@ -70,18 +70,18 @@ func TestParseBasics(t *testing.T) {
 		{
 			name: "priority repeats",
 			env:  map[string]string{"TEST_PARSE_S": "s.env", "TEST_PARSE_X": "x.env.1"},
-			file: "s s.file.1\ns s.file.2\n\nx x.file.1\nx x.file.2",
+			file: "testdata/5.conf",
 			args: []string{"-s", "s.arg.1", "-s", "s.arg.2", "-x", "x.arg.1", "-x", "x.arg.2"},
 			want: fftest.Vars{S: "s.arg.2", X: []string{"x.arg.1", "x.arg.2"}}, // highest prio wins and no others are called
 		},
 		{
 			name: "PlainParser solo bool",
-			file: "b\ns x\n",
+			file: "testdata/solo_bool.conf",
 			want: fftest.Vars{S: "x", B: true},
 		},
 		{
 			name: "PlainParser string with spaces",
-			file: "s i am the very model of a modern major general",
+			file: "testdata/spaces.conf",
 			want: fftest.Vars{S: "i am the very model of a modern major general"},
 		},
 	} {
@@ -89,9 +89,7 @@ func TestParseBasics(t *testing.T) {
 			var options []ff.Option
 
 			if testcase.file != "" {
-				filename, cleanup := fftest.TempFile(t, testcase.file)
-				defer cleanup()
-				options = append(options, ff.WithConfigFile(filename), ff.WithConfigFileParser(ff.PlainParser))
+				options = append(options, ff.WithConfigFile(testcase.file), ff.WithConfigFileParser(ff.PlainParser))
 			}
 
 			if len(testcase.env) > 0 {
@@ -114,37 +112,37 @@ func TestParseBasics(t *testing.T) {
 func TestParseIssue16(t *testing.T) {
 	for _, testcase := range []struct {
 		name string
-		file string
+		data string
 		want string
 	}{
 		{
 			name: "hash in value",
-			file: "s bar#baz",
+			data: "s bar#baz",
 			want: "bar#baz",
 		},
 		{
 			name: "EOL comment with space",
-			file: "s bar # baz",
+			data: "s bar # baz",
 			want: "bar",
 		},
 		{
 			name: "EOL comment no space",
-			file: "s bar #baz",
+			data: "s bar #baz",
 			want: "bar",
 		},
 		{
 			name: "only comment with space",
-			file: "# foo bar\n",
+			data: "# foo bar\n",
 			want: "",
 		},
 		{
 			name: "only comment no space",
-			file: "#foo bar\n",
+			data: "#foo bar\n",
 			want: "",
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
-			filename, cleanup := fftest.TempFile(t, testcase.file)
+			filename, cleanup := fftest.TempFile(t, testcase.data)
 			defer cleanup()
 
 			fs, vars := fftest.Pair()
