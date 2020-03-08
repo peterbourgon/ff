168dabc6dbd2df48b8c644c03f899270897fea0b
Peter Bourgon <peter@bourgon.org>
2020-03-08 23:05:28 +0100

    NoExecError: when terminal command has no func Exec

--- ffcli/command.go
+++ ffcli/command.go
@@ -66,18 +66,27 @@ type Command struct {
 	args     []string // args that should be passed to Run, if any
 
 	// Exec is invoked if this command has been determined to be the terminal
-	// command selected by the arguments provided to Run. The args passed to
-	// Exec are the args left over after flags parsing. Optional.
+	// command selected by the arguments provided to Parse or ParseAndRun. The
+	// args passed to Exec are the args left over after flags parsing. Optional.
 	//
-	// If Exec returns flag.ErrHelp, Run will behave as if -h were passed and
-	// emit the complete usage output.
+	// If Exec returns flag.ErrHelp, then Run (or ParseAndRun) will behave as if
+	// -h were passed and emit the complete usage output.
+	//
+	// If Exec is nil, and this command is identified as the terminal command,
+	// then Parse, Run, and ParseAndRun will all return NoExecError. Callers may
+	// check for this error and print e.g. help or usage text to the user, in
+	// effect treating some commands as just collections of subcommands, rather
+	// than being invocable themselves.
 	Exec func(ctx context.Context, args []string) error
 }
 
 // Parse the commandline arguments for this command and all sub-commands
-// recursively, defining flags along the way. If Parse returns without
-// an error, the terminal command has been successfully identified, and may
-// be invoked by calling Run.
+// recursively, defining flags along the way. If Parse returns without an error,
+// the terminal command has been successfully identified, and may be invoked by
+// calling Run.
+//
+// If the terminal command identified by Parse doesn't define an Exec function,
+// then Parse will return NoExecError.
 func (c *Command) Parse(args []string) error {
 	if c.selected != nil {
 		return nil
@@ -110,15 +119,20 @@ func (c *Command) Parse(args []string) error {
 	}
 
 	c.selected = c
+
+	if c.Exec == nil {
+		return NoExecError{Command: c}
+	}
+
 	return nil
 }
-
-// ErrUnparsed is returned by Run if Parse hasn't been called first.
-var ErrUnparsed = errors.New("command tree is unparsed, can't run")
 
 // Run selects the terminal command in a command tree previously identified by a
 // successful call to Parse, and calls that command's Exec function with the
 // appropriate subset of commandline args.
+//
+// If the terminal command previously identified by Parse doesn't define an Exec
+// function, then Run will return NoExecError.
 func (c *Command) Run(ctx context.Context) (err error) {
 	var (
 		unparsed = c.selected == nil
@@ -138,7 +152,7 @@ func (c *Command) Run(ctx context.Context) (err error) {
 	case terminal:
 		return c.Exec(ctx, c.args)
 	case noop:
-		return nil
+		return NoExecError{Command: c}
 	default:
 		return c.selected.Run(ctx)
 	}
@@ -158,6 +172,28 @@ func (c *Command) ParseAndRun(ctx context.Context, args []string) error {
 
 	return nil
 }
+
+//
+//
+//
+
+// ErrUnparsed is returned by Run if Parse hasn't been called first.
+var ErrUnparsed = errors.New("command tree is unparsed, can't run")
+
+// NoExecError is returned if the terminal command selected during the parse
+// phase doesn't define an Exec function.
+type NoExecError struct {
+	Command *Command
+}
+
+// Error implements the error interface.
+func (e NoExecError) Error() string {
+	return fmt.Sprintf("terminal command (%s) doesn't define an Exec function", e.Command.Name)
+}
+
+//
+//
+//
 
 // DefaultUsageFunc is the default UsageFunc used for all commands
 // if no custom UsageFunc is provided.
