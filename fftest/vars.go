e3f2dac1eacfd02d8731f5969b38396178ced17c
Aram Openden <Aram.Openden1@ibm.com>
2021-06-25 15:05:58 -0400

    added new public function comment
    Signed-off-by: Aram Openden <Aram.Openden1@ibm.com>

--- fftest/vars.go
+++ fftest/vars.go
@@ -27,17 +27,20 @@ func Pair() (*flag.FlagSet, *Vars) {
 	return fs, &v
 }
 
+// EmptyValsFS returns a predefined flag set, and a predefined set of variables that
+// have been registered into it.
+// Custom Test Flagset creation function for the Empty (YAML) Values parsing, using more
+// specific flag names.
 func EmptyValsFS() (*flag.FlagSet, *Vars) {
 	fs := flag.NewFlagSet("ffemptytest", flag.ContinueOnError)
 
-	//Set Defaults to be something OTHER than GoLang defaults for each type
 	var v Vars
 	fs.StringVar(&v.S, "emptyStr", "", "string")
 	fs.IntVar(&v.I, "emptyInt", 0, "int")
 	fs.Float64Var(&v.F, "emptyFloat", 0.00, "float64")
 	fs.BoolVar(&v.B, "emptyBool", false, "bool")
 	fs.DurationVar(&v.D, "emptyDur", 0*time.Second, "time.Duration")
-	fs.Var(&v.X, "defaultSlice", "collection of strings (repeatable)")
+	fs.Var(&v.X, "emptySlice", "collection of strings (repeatable)")
 
 	return fs, &v
 }
